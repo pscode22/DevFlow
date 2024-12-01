3b2bb5592b2cf91c16ed24f3004940a68e8d4fa2
Praveen <111781869+pscode22@users.noreply.github.com>
2024-12-01 20:48:19 +0530

    Auth Setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Navbar from "@/components/navigation/navbar";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default RootLayout;
