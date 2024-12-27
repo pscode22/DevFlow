dfb365712a2f1d07dcc8f268fcef40ef1003afa9
Praveen <111781869+pscode22@users.noreply.github.com>
2024-12-27 13:59:11 +0530

    Implement RightSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 
+import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
+import RightSidebar from "@/components/navigation/RightSidebar";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <main className="background-light850_dark100 relative">
       <Navbar />
-      {children}
-    </div>
+
+      <div className="flex">
+        <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+
+        <RightSidebar />
+      </div>
+    </main>
   );
 };
 
